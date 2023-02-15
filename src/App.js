bc10470b4264c7be07a2c3102924d89e178f5e95
mosab01 <mosabkhraim3@gmail.com>
2023-02-15 20:54:09 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,57 +1,82 @@
 import "./App.css";
 import { useState } from "react";
-import clouds from ".images/clouds.jpg";
+import clouds from "./images/clouds.jpg";
 
 // var city= document.getElementById("city-name").value;
-var API_KEY = "ab1b212ce4cc6959926569bd71264a61";
-
 
 function App() {
   const [city, setCity] = useState("");
 
 const set_city = () =>{
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ab1b212ce4cc6959926569bd71264a61`)
   .then(response => response.json())
   .then(json => {
-    console.log(json.weather[0].main);
-    console.log(json.main.temp);
+
+
+
+    
+    
     document.getElementById("temputre").innerHTML = json.main.temp;
-  });  
+    document.getElementById("city-name").innerHTML = json.name;
+    document.getElementById("sun-rise").innerHTML = `Sun-rise = ${unix_to_time(json.sys.sunrise)}`;
+    document.getElementById("sun-set").innerHTML = `Sun-set = ${unix_to_time(json.sys.sunset)}`;
+    document.getElementById("min-temp").innerHTML = `min temp = ${json.main.temp_min}`;
+    document.getElementById("max-temp").innerHTML = `max temp = ${json.main.temp_max}`;
+
+  });
 }
+
+const unix_to_time = (time) =>{
+  var date = new Date(time * 1000);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+  
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+  return(formattedTime);
+}
+
 
   return (
     <div className="App">
       <div className="input-city-name">
       <input 
-      id='city-name'
+      id='get-city-name'
       placeholder='City Name'
       value={city}
       onChange={(m) => setCity(m.target.value)}
       />
-
       <button
           alt="search"onClick={() => set_city(city)}>
-            click me</button>
+            click me
+      </button>
+      </div>
 
-    </div>
+      <div className="weather">
+        <div className="city-date">
+          <h1 id="city-name"></h1>
+          <h2 id="date-time">Mon 12th jan</h2>
+        </div>
+        <div className="picture">
+          <img id="weather-img" src={clouds} alt="clouds"/>
+        </div>
+        <div className="temp">
+          <p id="temputre"></p>
+          <p>°C</p>
+        </div>
+        <div className="other-weather">
+          <p id="sun-rise"></p>
+          <p id="sun-set"></p>
+          <p id="min-temp"></p>
+          <p id="max-temp"></p>
 
-    <div className="weather">
-      <div className="city-date">
-      <h1 id="city-name">{city}</h1>
-      <h2 id="date-time">Mon 12th jan</h2>
+        </div>
       </div>
-      <div className="picture">
-        <img src={clouds} alt="clouds"/>
-      </div>
-      <div className="temp">
-        <h1 id="temputre"><span>°C</span></h1>
-      </div>
-      <div className="other-weather">
-        <button>btn-1</button>
-        <button>btn-2</button>
-        <button>btn-3</button>
-      </div>
-    </div>
     </div>
   );
 }
@@ -83,13 +108,17 @@ export default App;
 // "visibility":10000,
 // "wind":{"speed":3.09,
 // "deg":220,"gust":8.75},
+
 // "clouds":{"all":100},
+
 // "dt":1676457152,
+
 // "sys":{"type":2,
 // "id":2075535,
 // "country":"GB",
 // "sunrise":1676445323,
 // "sunset":1676481250},
+
 // "timezone":0,
 // "id":2643743,
 // "name":"London",
